360aabee6b9aa20cd5e35b9201cc2890f0ee42b7
madisonsilver <sir.seldom.vain@gmail.com>
2017-08-15 19:24:16 -0700

    Added canvas folder
    //TODO: Actually do this

---
+++ canvas/engine.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById('playField');
+function drawFrame(){
+if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    // drawing code here
+} else {
+    alert("Your browser doesn't seem to support <canvas>.")
+}
+}
